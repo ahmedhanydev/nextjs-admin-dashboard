042bc68bdb637f5ae54ca93cb5f9d30ba06d494d
ahmedhany <elgenius10000@gmail.com>
2024-08-05 11:37:23 +0300

    add: account details for test

--- app/ui/login/loginForm/loginForm.tsx
+++ app/ui/login/loginForm/loginForm.tsx
@@ -6,13 +6,18 @@ import { useFormState } from "react-dom";
 const LoginForm = () => {
   const [state, formAction] = useFormState(authenticate, undefined);
   return (
-    <form action={formAction} className={styles.form}>
-      <h1>Login</h1>
-      <input type="text" placeholder="username" name="username" />
-      <input type="password" placeholder="password" name="password" />
-      <button>Login</button>
-      {state && state}
-    </form>
+    <>
+      <form action={formAction} className={styles.form}>
+        <h1>Login</h1>
+        <h3>Account For Test :</h3>
+        <h4 className={styles.username}>username:ahmed123</h4>
+        <h4 className={styles.username}>password:ahmed123</h4>
+        <input type="text" placeholder="username" name="username" />
+        <input type="password" placeholder="password" name="password" />
+        <button>Login</button>
+        {state && state}
+      </form>
+    </>
   );
 };
 
